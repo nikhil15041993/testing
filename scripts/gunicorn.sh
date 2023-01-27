56721711178136f08eec88b3dd9a03d25ed3e5e2
NIKHIL S NAMBIAR <87381349+nikhil15041993@users.noreply.github.com>
2023-01-27 11:41:25 +0530

    Update gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -2,7 +2,7 @@
 
 source env/bin/activate
 
-cd /var/lib/jenkins/workspace/django-cicd/app
+cd /var/lib/jenkins/workspace/python-ci-cd/app
 
 python3 manage.py makemigrations
 python3 manage.py migrate
